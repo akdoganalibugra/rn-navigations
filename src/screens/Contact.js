b5e5f6ee64e62ccd48c3c43d17978330a8df1e97
akdoganalibugra1 <akdoganalibugra@gmail.com>
2024-03-20 23:46:20 +0300

    Stack Navigation

---
+++ src/screens/Contact.js
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Contact = () => {
+  return (
+    <View>
+      <Text>This is the Contact Screen</Text>
+    </View>
+  );
+};
+
+export default Contact;
+
+const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+  },
+});
---
+++ src/screens/Home.js
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View, Button } from "react-native";
+import React from "react";
+
+const Home = ({ navigation }) => {
+  return (
+    <View style={styles.center}>
+      <Text>This is the home screen</Text>
+      <Button
+        title="Go to About Screen"
+        onPress={() => navigation.navigate("About")}
+      />
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+  },
+});
